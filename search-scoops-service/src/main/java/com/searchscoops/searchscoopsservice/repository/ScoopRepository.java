07eb3044f7dcbe4b5c1c310be16e3b3ea4310a29
Esha Chandaliya <88758911+eshaongit@users.noreply.github.com>
2023-07-27 11:56:09 +0530

    Delete search-scoops-service directory

--- search-scoops-service/src/main/java/com/searchscoops/searchscoopsservice/repository/ScoopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.searchscoops.searchscoopsservice.repository;
-
-import com.searchscoops.searchscoopsservice.model.Scoop;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ScoopRepository extends MongoRepository<Scoop, String> {
-}
